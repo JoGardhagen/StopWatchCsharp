287bafd8fa84576dac128963eb8ced51275c240d
Joakim <superapa.spanx@gmail.com>
2022-04-28 20:11:03 +0200

    init

---
+++ StopWatch/ViewController.designer.cs
@@ -0,0 +1,29 @@
+// WARNING
+//
+// This file has been generated automatically by Visual Studio to store outlets and
+// actions made in the UI designer. If it is removed, they will be lost.
+// Manual changes to this file may not be handled correctly.
+//
+using Foundation;
+using System.CodeDom.Compiler;
+
+namespace StopWatch
+{
+	[Register ("ViewController")]
+	partial class ViewController
+	{
+		[Outlet]
+		AppKit.NSTextField clock { get; set; }
+
+		[Action ("startButton:")]
+		partial void startButton (Foundation.NSObject sender);
+		
+		void ReleaseDesignerOutlets ()
+		{
+			if (clock != null) {
+				clock.Dispose ();
+				clock = null;
+			}
+		}
+	}
+}
