90b48e896b6a6edcf92eea2f58bafe82a0a35832
Joakim <superapa.spanx@gmail.com>
2022-04-28 20:25:28 +0200

    new class

---
+++ StopWatch/Time.cs
@@ -0,0 +1,63 @@
+﻿using System;
+namespace stopWatchCsharp
+{
+    public class Time
+    {
+        private int hour;
+        private int minute;
+        private int second;
+
+
+        public Time(int hour, int minute, int second)
+        {
+            this.hour = hour;
+            this.minute = minute;
+            this.second = second;
+        }
+        public Time(string currentTime)
+        {
+            string[] time = currentTime.Split(":");
+            hour = int.Parse(time[0]);
+            minute = int.Parse(time[1]);
+            second = int.Parse(time[2]);
+        }
+        public string getCurrentTime()
+        {
+            return hour + ":" + minute + ":" + second;
+        }
+        public string timeReset(string currentTime)
+        {
+            string[] time = currentTime.Split(":");
+            hour = int.Parse(time[0]);
+            minute = int.Parse(time[1]);
+            second = int.Parse(time[2]);
+
+            //hour = Integer.parseInt(time[0]);
+            //minute = Integer.parseInt(time[1]);
+            //second = Integer.parseInt(time[2]);
+            hour = 0;
+            minute = 0;
+            second = 0;
+            return hour + ":" + minute + ":" + second;
+        }
+        public void oneSecondPassed()
+        {
+            second++;
+            if (second == 60)
+            {
+                minute++;
+                second = 0;
+                if (minute == 60)
+                {
+                    hour++;
+                    minute = 0;
+                    if (hour == 24)
+                    {
+                        hour = 0;
+                        Console.WriteLine("Next day");
+                    }
+                }
+            }
+        }
+    }
+}
